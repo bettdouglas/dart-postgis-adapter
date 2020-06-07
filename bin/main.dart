fcd14821deecb35c5da656c3d1fe64e489f8b16d
bettdouglas <bettdalpha@gmail.com>
2020-06-07 18:49:47 +0300

    updated postgres-dart to fork

---
+++ bin/main.dart
@@ -0,0 +1,36 @@
+import 'package:hospitals_jts/alaska/alaska-db-adapter.dart';
+import 'package:postgres/postgres.dart';
+
+void main(List<String> arguments) async {
+  var connection = PostgreSQLConnection(
+    'localhost',
+    5432,
+    'alaska',
+    username: 'postgres',
+    password: 'Mula1000',
+  );
+
+  await connection.open();
+
+  final askAlaska = AskAlaska(connection);
+
+  final rivers = await askAlaska.getAlaskaRivers();
+  // print(rivers.length);
+  final airports = await askAlaska.getAlaskaAirports();
+  final lakes = await askAlaska.getAlaskaLakes();
+
+  // rivers.forEach((river) { 
+
+  //   var riverLength = river.geometry.getLength();
+  //   print('River ${river.name} has length of $riverLength');
+  // });
+
+  // lakes.forEach((lake) {
+  //   var lakeArea = lake.geometry.getArea();
+  //   var srid = lake.geometry.getSRID();
+  //   print('Lake ${lake.name} has an area of $lakeArea using SRID $srid');
+  // });
+
+  // run queries and add more queries into AskAlaska
+  await connection.close();
+}
--- lib/alaska/alaska-db-adapter.dart
+++ lib/alaska/alaska-db-adapter.dart
@@ -1,5 +1,4 @@
 import 'package:hospitals_jts/alaska/models/models.dart';
-import 'package:hospitals_jts/postgis_wrapper/ewkt_parser.dart';
 import 'package:postgres/postgres.dart';
 import 'package:dart_jts/dart_jts.dart';
 
@@ -10,30 +9,30 @@ class AskAlaska {
   AskAlaska(this.connection);
 
   Future<List<River>> getAlaskaRivers() async {
-    var queryString = 'SELECT gid,f_codedesc,nam,f_code,ST_AsEWKB(geom) FROM rivers'; 
+    var queryString = 'SELECT gid,f_codedesc,nam,f_code,geom FROM rivers'; 
     var results = await connection.query(queryString);
     return results.map(dbMapper.mapRiverData).toList();
   }
 
   Future<List<Lake>> getAlaskaLakes() async {
-    var queryString = 'SELECT gid,names,ST_AsEWKB(geom) from lakes';
+    var queryString = 'SELECT gid,names,geom from lakes';
     var results = await connection.query(queryString);
     return results.map(dbMapper.mapLakeData).toList();
   }
 
   Future<List<Airport>> getAlaskaAirports() async {
-    var queryString = 'SELECT gid,id,fk_region,elev::REAL,name,use,ST_AsEWKB(geom) FROM airports';
+    var queryString = 'SELECT gid,id,fk_region,elev::REAL,name,use,geom FROM airports';
     var results = await connection.query(queryString);
     return results.map(dbMapper.mapAirportData).toList();
   }
 
   Future<bool> insertGeom(Geometry geom) async {
-    var ewkt = WKBWriter().write(geom);
-    print(ewkt.toString());
+    // var ewkt = WKBWriter().write(geom);
+    // print(ewkt.toString());
     var statement = 'INSERT INTO geoms(geom) values(@wkb)';
     try {
       await connection.execute(statement,substitutionValues: {
-        'wkb' : ewkt
+        'wkb' : geom.toText()
       });
       return true;
     } catch (e) {
@@ -45,21 +44,21 @@ class AskAlaska {
 
 class AlaskaDBMapper {
 
-  final pgParser = PostgisEWKTParser();
+  // final pgParser = PostgisEWKTParser();
 
   River mapRiverData(PostgreSQLResultRow row) {
-    var riverGeom = pgParser.parseMultiLineString(row[4]);
-    return River(row[0], row[1], row[2], row[3],riverGeom);
+    // var riverGeom = pgParser.parseMultiLineString(row[4]);
+    return River(row[0], row[1], row[2], row[3],row[4]);
   }
 
   Lake mapLakeData(PostgreSQLResultRow row) {
-    var lakeGeom = pgParser.parseMultiPolygon(row[2]);
-    return Lake(row[0], row[1], lakeGeom);
+    // var lakeGeom = pgParser.parseMultiPolygon(row[2]);
+    return Lake(row[0], row[1], row[2]);
   }
 
   Airport mapAirportData(PostgreSQLResultRow row) {
-    var airportLocation = pgParser.parsePoint(row[6]);
-    return Airport(row[0],row[1],row[2],row[3],row[4],row[5],airportLocation);
+    // var airportLocation = pgParser.parsePoint(row[6]);
+    return Airport(row[0],row[1],row[2],row[3],row[4],row[5],row[6]);
   }
 
 
