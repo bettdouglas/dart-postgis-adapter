f43ac4b68f997bb6d1559c057a16836f20b613bd
bettdouglas <bettdalpha@gmail.com>
2020-06-14 11:48:03 +0300

    test

--- Dockerfiles/db/import.sh
+++ Dockerfiles/db/import.sh
@@ -5,10 +5,10 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER dart;
     CREATE DATABASE dart_test;
-    GRANT ALL PRIVILEGES ON DATABASE dart_test TO mrdoug;
+    GRANT ALL PRIVILEGES ON DATABASE dart_test TO dart;
 EOSQL
 
-for entry in "../../lib/alaska-data"
+for entry in "/tmp/shapefiles/*.shp"
 do
     echo "$entry"
 done
