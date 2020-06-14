ae381b778b585f453a7fb2ba399c37b90f4f2f30
bettdouglas <bettdalpha@gmail.com>
2020-06-14 11:54:43 +0300

    using bash dockere

--- Dockerfiles/db/import.sh
+++ Dockerfiles/db/import.sh
@@ -1,14 +1,14 @@
   
 #!/bin/bash
-set -e
+# set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER dart;
-    CREATE DATABASE dart_test;
-    GRANT ALL PRIVILEGES ON DATABASE dart_test TO dart;
-EOSQL
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+#     CREATE USER dart;
+#     CREATE DATABASE dart_test;
+#     GRANT ALL PRIVILEGES ON DATABASE dart_test TO dart;
+# EOSQL
 
-for entry in "/tmp/shapefiles/*.shp"
+for entry in /tmp/shapefiles/*.shp
 do
     echo "$entry"
 done
